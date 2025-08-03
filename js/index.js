9468e19d4b557f450b73cc2fb7ab30472a4a2b6a
sthephaniplatero <122330814+sthephaniplatero@users.noreply.github.com>
2025-08-02 22:07:21 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `en tu cumple las flores no pueden faltar !`.split('')
+const text = `Te amo !`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
